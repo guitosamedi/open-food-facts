d771b34fba224bce1a8d53e34c8895925d0ba745
lucas PREAUX <lucas.preau@gmail.com>
2023-06-23 09:59:19 +0200

    Implements entities : Additif, Allergene,Ingredient, MArque

---
+++ app-off/src/main/java/fr/diginamic/entite/Ingredient.java
@@ -0,0 +1,39 @@
+package fr.diginamic.entite;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(length=100)
+    private String nom;
+
+    public Ingredient() {}
+
+    public Ingredient(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
